9445324ec7f1530f19988e0c0b62874680925c70
Al- Ekram Hossain Abir <alabir65@gmail.com>
2025-09-02 15:28:56 +0600

    add package

---
+++ packages/ui/Button.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Button as AntButton } from "antd";
+
+export const Button = () => {
+  return <AntButton type="primary">Shared Button</AntButton>;
+};
---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "../../apps/frontend/**/*.{js,ts,jsx,tsx}",
+    "../../apps/landing/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
